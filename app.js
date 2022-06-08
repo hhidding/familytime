5cd3459d5c7515b34e453485da0ac2c9b359158f
Henri Hidding <hhidding@gmail.com>
2022-06-08 20:38:11 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,254 @@
+window.onload = function() {
+    initAnimations();
+    test = window.requestAnimationFrame(loop);
+};
+
+const nrs = [ 
+    [[1,0], [2,0], [0,1], [3,1], [0,2], [3,2], [0,3], [3,3], [0,4], [3,4], [1,5], [2,5]],               //0
+    [[1,0], [0,1], [1,1], [1,2], [1,3], [1,4], [0,5], [1,5], [2,5]],                                    //1
+    [[1,0], [2,0], [0,1], [3,1], [2,2], [1,3], [0,4], [0,5], [1,5], [2,5], [3,5]],                      //2
+    [[0,0], [1,0], [2,0], [3,0], [3,1], [2,2], [3,3], [0,4], [3,4], [1,5], [2,5]],                      //3
+    [[0,0], [0,1], [0,2], [2,2], [0,3], [1,3], [2,3], [3,3], [2,4], [2,5]],                             //4
+    [[0,0], [1,0], [2,0], [3,0], [0,1], [0,2], [1,2], [2,2], [3,3], [0,4], [3,4], [1,5], [2,5]],        //5
+    [[1,0], [2,0], [0,1], [0,2], [1,2], [2,2], [0,3], [3,3], [0,4], [3,4], [1,5], [2,5]],               //6
+    [[0,0], [1,0], [2,0], [3,0], [3,1], [2,2], [2,3], [1,4], [1,5]],                                    //7
+    [[1,0], [2,0], [0,1], [3,1], [1,2], [2,2], [0,3], [3,3], [0,4], [3,4], [1,5], [2,5]],               //8
+    [[1,0], [2,0], [0,1], [3,1], [0,2], [3,2], [1,3], [2,3], [3,3], [3,4], [1,5], [2,5]]                //9
+];
+
+const colon = [[ 0,1] , [ 0,3]];                                                                        //:
+const colonOffset1 = [500,0];
+const colonOffset2 = [1100,0];
+const screenOffsetX = 100;
+const screenOffsetY = 200;
+const offset = [
+    [0,0] , [250,0] , [600,0] , [850,0], [1200,0] , [1450,0]
+]
+
+const width=51;
+const height=78;
+const canvas = document.getElementById('root')
+    
+const btn = document.getElementById('btn')
+const ctx = canvas.getContext('2d')
+const photodir = 'photos/';
+const PHOTO_COUNT=1;
+const animationDuration = 500;
+
+var faces = [];
+var clockFaces = [];
+var face;
+var test;
+var animate=[0,0,0,0,0,0];
+var previous=[0,0,0,0,0,0];
+
+const STYLE_NONE=0;
+const STYLE_FADE=1;
+const STYLE_TRAVEL=2;
+
+class AnimationStyle {
+    constructor(name, duration) {
+        this.name = name;
+        this.duration = duration;
+    }
+}
+
+var animations = {};
+function initAnimations() {
+    animations[0] = new AnimationStyle(STYLE_TRAVEL,8000);
+    animations[1] = new AnimationStyle(STYLE_TRAVEL,8000);
+    animations[2] = new AnimationStyle(STYLE_TRAVEL,4000);
+    animations[3] = new AnimationStyle(STYLE_TRAVEL,4000);
+    animations[4] = new AnimationStyle(STYLE_TRAVEL,2000);
+    animations[5] = new AnimationStyle(STYLE_FADE,500);
+
+    var date = new Date();
+    for(var i=0;i<=6;i++) {
+        previous[i]= getClockInteger(i,date);
+    }
+
+    loadPhotos();
+    initClockFaces();
+}
+
+function loadPhotos() {
+    for(var i=0;i<PHOTO_COUNT; i++) {
+        var face  = new Image();
+        face.src = photodir + i + '.png';
+        faces[i] = face;
+    }
+    face = faces[0];
+}
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+function initClockFaces() {
+    for(var nr=0;nr<6;nr++) {
+        var faces= [];
+        for(var f=0;f<=13;f++) {
+            faces[f] = getRandomInt(PHOTO_COUNT);
+        }
+        clockFaces[nr] = faces;
+    }
+}
+
+function drawTime() { 
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawColon();
+    var date = new Date();
+    for(var i=0;i<6;i++ ) {
+        drawPostion(i,date);
+    }
+}
+
+function drawPostion(pos, date) {
+    var clockNr = getClockInteger(pos,date);
+    var animation = animations[pos];
+
+    var prevClockNr = previous[pos];
+
+    if (prevClockNr != clockNr && animate[pos]==0) {
+        animate[pos]=date.getTime() + animation.duration;
+    }
+
+    if(prevClockNr != clockNr) {
+        var diff = animate[pos] - date.getTime();
+        if (diff > 0) {
+            var perc = 1 - (diff / animation.duration);
+            animateNr(pos, prevClockNr, clockNr, perc, animation);
+        } else {
+            previous[pos]=clockNr;
+            animate[pos]=0;
+            drawNr(pos, clockNr);
+        }
+    } else {
+        drawNr(pos, clockNr);
+    }
+}
+
+function getClockInteger(pos,date) {
+    var h = date.getHours();
+    var m = date.getMinutes();
+    var s = date.getSeconds();
+
+    var clockNr;
+    switch(pos) {
+        case 0:
+            clockNr = Math.floor(h / 10);
+            break;
+        case 1:
+            clockNr = h % 10;
+            break;
+        case 2:
+            clockNr = Math.floor(m / 10);
+            break;
+        case 3:
+            clockNr = m % 10;
+            break;
+        case 4:
+            clockNr = Math.floor(s / 10);
+            break;
+        case 5:
+            clockNr = s % 10;
+            break;
+    }
+    return clockNr;
+}
+
+function loop(){ 
+    drawTime();
+    test = window.requestAnimationFrame(loop)   // Needed to keep looping
+}   
+
+function drawNr(pos, clockDigit) {
+    var image_offset = offset[pos];
+    nr = nrs[clockDigit];
+    for(var i=0;i<nr.length;i++) {
+        face = faces[clockFaces[pos][i]];
+        drawImageAt(image_offset, nr[i]);
+    }
+}
+
+function animateNr(pos, fromDigit, toDigit, perc, animation) {
+    var image_offset = offset[pos];
+    from = nrs[fromDigit];
+    to = nrs[toDigit];
+    var maxnr = Math.max(from.length,to.length);
+   
+    for(var i=0;i<maxnr;i++) {
+        face = faces[clockFaces[pos][i]];
+        var pf=null;
+        var pt=null;
+        if(i<from.length) {
+            pf = from[i];
+        }
+        if(i<to.length) {
+            pt = to[i];
+        }
+        if (animation.name == STYLE_FADE) {
+            fadeImageAt(image_offset, pf,pt, perc);
+        } else if (animation.name == STYLE_TRAVEL) {
+            travelImageAt(image_offset, pf,pt, perc);
+        }
+    }
+}
+
+function drawColon() {
+    var s = new Date().getMilliseconds();
+    if(s > 500) {
+        face = faces[clockFaces[0][12]];
+        colon.forEach( element => drawImageAt(colonOffset1, element) );
+        face = faces[clockFaces[1][12]];
+        colon.forEach( element => drawImageAt(colonOffset2, element) );
+    }
+}
+
+function drawImageAt(offset, p) {
+    x = p[0] * width + offset[0] + screenOffsetX;
+    y = p[1] * height + offset[1] + screenOffsetY;
+    ctx.drawImage(face, x, y);
+}
+
+function fadeImageAt(offset, pf,pt, perc) {
+    if(pf !=null ) {
+        ctx.globalAlpha = 1-perc;
+        x = pf[0] * width + offset[0] + screenOffsetX;
+        y = pf[1] * height + offset[1] + screenOffsetY;
+        ctx.drawImage(face, x, y);
+    }
+    
+    if(pt !=null ) {
+        ctx.globalAlpha = perc;
+        x = pt[0] * width + offset[0] + screenOffsetX;
+        y = pt[1] * height + offset[1] + screenOffsetY;
+        ctx.drawImage(face, x, y);
+    }
+    ctx.globalAlpha = 1;
+}
+
+function travelImageAt(offset, pf,pt, perc) {
+    if(pf ==null ) {
+        ctx.globalAlpha = perc;
+        fx = pt[0] * width + offset[0] + screenOffsetX;
+        fy = pt[1] * height + offset[1] + screenOffsetY;
+    } else {
+        fx = pf[0] * width + offset[0] + screenOffsetX;
+        fy = pf[1] * height + offset[1] + screenOffsetY;
+    }
+    
+    if(pt ==null ) {
+        ctx.globalAlpha = 1-perc;
+        tx = fx;
+        ty = fy;
+    } else {
+        tx = pt[0] * width + offset[0] + screenOffsetX;
+        ty = pt[1] * height + offset[1] + screenOffsetY;
+    }
+
+    x = fx + (tx-fx) * perc; 
+    y = fy + (ty-fy) * perc; 
+    ctx.drawImage(face, x, y);
+    ctx.globalAlpha = 1;
+}
